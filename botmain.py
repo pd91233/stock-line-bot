b5615281f9fc9a209ac1fe756a18e4dfb9b11650
pd91233 <pd91233@gmail.com>
2026-09-23 11:47:44 +0900

    Update botmain.py

--- botmain.py
+++ botmain.py
@@ -3253,7 +3253,15 @@ def continuous_radar_loop():
 
                         # 接收即時成交報價
                         if event == "data":
-                            quote = msg_data.get("data", {})
+
+                        print(
+                    
+                            f"📦 [原始封包] {str(msg_data)[:500]}",
+                    
+                            flush=True
+                    
+                        )
+                            quote = msg_data.get you("data", {})
                             code = quote.get("symbol", "")
 
                             print(
